dff0f8b2004ac3bfea5ea4d9c2d03152dc0d3c30
Alex Shumilov <ashumilov@pivotal.io>
2018-05-07 08:28:25 -0400

    Move cache configurations here.

--- src/main/java/io/microsamples/cache/admin/config/CacheAdminAutoConfiguration.java
+++ src/main/java/io/microsamples/cache/admin/config/CacheAdminAutoConfiguration.java
@@ -1,14 +1,89 @@
 package io.microsamples.cache.admin.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
-//todo figure this out
-//@ConditionalOnBean(name = {"redisTemplate", "keyRedisTemplate", "cacheManager"})
-@ComponentScan({"io.microsamples.cache.admin.api"})
+@ComponentScan({"io.microsamples.cache.admin"})
 @Slf4j
-public class CacheAdminAutoConfiguration {
+public class CacheAdminAutoConfiguration extends CachingConfigurerSupport {
+
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                List<String> methodParams = new ArrayList<>();
+                for (Object param : params) {
+                    methodParams.add(param.toString());
+                }
+
+                CacheKey key = CacheKey.builder()
+                        .className(target.getClass().toString())
+                        .method(method.toString())
+                        .params(methodParams)
+                        .build();
+
+                ObjectMapper objectMapper = new ObjectMapper();
+
+                try {
+                    return objectMapper.writeValueAsString(key);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException("Not able to process key for cache", e);
+                }
+
+            }
+        };
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Bean
+    @ConditionalOnMissingBean
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
+        rcm.setDefaultExpiration(600); //<-- expire cache every 10 minutes
+        return rcm;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> keyRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+
 }
